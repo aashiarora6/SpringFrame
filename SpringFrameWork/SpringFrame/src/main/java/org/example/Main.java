1500a8e397c1092b947f5afe13c77dae23283b21
ashiarora6 <aashiarora6666@gmail.com>
2024-03-04 15:04:58 +0530

    "Doctor and Nurse"

---
+++ SpringFrameWork/SpringFrame/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Doctor d1 = context.getBean(Doctor.class);
+        d1.assist();
+
+        Nurse n1 = (Nurse) context.getBean("nurse");
+        n1.assist();
+    }
+}
