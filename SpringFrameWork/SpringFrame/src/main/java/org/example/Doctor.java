b2a4cd35b4abd4e776b1faec7405d5bbc81a4160
ashiarora6 <aashiarora6666@gmail.com>
2024-03-04 15:04:04 +0530

    "Doctor and Nurse"

---
+++ SpringFrameWork/SpringFrame/src/main/java/org/example/Doctor.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Doctor {
+    public void assist(){
+        System.out.println("Doctor is Assisting");
+    }
+}
---
+++ SpringFrameWork/SpringFrame/src/main/java/org/example/Nurse.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Nurse {
+    public void assist(){
+        System.out.println("Nurse is Assisting");
+    }
+}
