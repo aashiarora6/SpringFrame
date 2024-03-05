defdc43c9891598cb8d207a983cac02fc7dab0c6
ashiarora6 <aashiarora6666@gmail.com>
2024-03-05 11:27:24 +0530

    "Only one object is created so the output for both d1 and d2 is 'MBBS', the scope is singelton by default, i.e. a single scope was created"

--- SpringFrameWork/SpringFrame/src/main/java/org/example/Doctor.java
+++ SpringFrameWork/SpringFrame/src/main/java/org/example/Doctor.java
@@ -5,7 +5,21 @@ import org.springframework.stereotype.Component;
 
 public class Doctor implements  Staff {
 
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "qualification='" + qualification + '\'' +
+         '}';
+    }
+    private String qualification;
     public void assist(){
         System.out.println("Doctor is Assisting");
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+    public String getQualification() {
+        return qualification;
     }
 }
--- SpringFrameWork/SpringFrame/src/main/java/org/example/Main.java
+++ SpringFrameWork/SpringFrame/src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.print.Doc;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,5 +15,11 @@ public class Main {
         ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
         Doctor d1 = context.getBean(Doctor.class);
         d1.assist();
+        d1.setQualification("MBBS");
+        System.out.println(d1);
+
+        Doctor d2 = context.getBean(Doctor.class);
+        System.out.println(d2);
+
        }
 }
