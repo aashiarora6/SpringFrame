33c6603fef7330a1309aee636f728d120fe5eec2
ashiarora6 <aashiarora6666@gmail.com>
2024-03-05 11:15:04 +0530

    "Removed annotation Component from Doctor and Using Bean in BeanConfig class, and creating a bean for doctor."

--- SpringFrameWork/SpringFrame/src/main/java/org/example/BeanConfig.java
+++ SpringFrameWork/SpringFrame/src/main/java/org/example/BeanConfig.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
 public class BeanConfig {
+
+    @Bean
+    public Doctor doctor(){
+        return new Doctor();
+    }
 
 }
--- SpringFrameWork/SpringFrame/src/main/java/org/example/Doctor.java
+++ SpringFrameWork/SpringFrame/src/main/java/org/example/Doctor.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Doctor implements  Staff {
 
     public void assist(){
