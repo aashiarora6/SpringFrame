9074db58c763c7506abf45193acc626f7cfc456a
ashiarora6 <aashiarora6666@gmail.com>
2024-03-23 20:08:29 +0530

    "Rest Api returns List<Student> "

--- SpringBootApp1/springboot-first-app/src/main/java/com/springboot/app/StudentController.java
+++ SpringBootApp1/springboot-first-app/src/main/java/com/springboot/app/StudentController.java
@@ -4,11 +4,25 @@ package com.springboot.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
     @GetMapping("/student")
     public Student getStudent(){
         return new Student("Aashi", "Arora");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Aashi", "Arora"));
+        students.add(new Student("Arnav", "Arora"));
+        students.add(new Student("Raju", "Arora"));
+        students.add(new Student("Rishabh", "Arora"));
+        students.add(new Student("Nupur", "Arora"));
+        return students;
     }
 }
