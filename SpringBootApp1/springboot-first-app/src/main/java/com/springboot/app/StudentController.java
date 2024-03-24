b1cfba442034fd8feab43faa234d549705482c99
ashiarora6 <aashiarora6666@gmail.com>
2024-03-24 12:04:23 +0530

    "Rest API with query Parameters"

--- SpringBootApp1/springboot-first-app/src/main/java/com/springboot/app/StudentController.java
+++ SpringBootApp1/springboot-first-app/src/main/java/com/springboot/app/StudentController.java
@@ -3,6 +3,7 @@ package com.springboot.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -29,6 +30,14 @@ public class StudentController {
 
     @GetMapping("/student/{firstName}/{lastName}")
     public Student studentPathVariable(@PathVariable("firstName") String firstName,@PathVariable("lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
+
+
+    @GetMapping("/student/query")
+    public Student studentQueryParam(
+            @RequestParam(name = "firstName") String firstName,
+            @RequestParam(name = "lastName") String lastName){
         return new Student(firstName, lastName);
     }
 }
